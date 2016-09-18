7ec13e29637b1afa40ec81dd860516c5bac2bdb7
Alexandre SUY <alexandre.suy@gmail.com>
2016-09-19 00:37:51 +0200

    Add code and game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertival = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertival);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
